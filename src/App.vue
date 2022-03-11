4d1930a173d84c57a9efea7340423ef163b4229d
pranjaypathak411 <pranjaypathak411@gmail.com>
2022-03-11 10:56:25 +0530

    Add Token list to fetch their balance

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
     </div>
     <div v-else>
       <div class="balance_dashboard">
-        <button v-if="balance == null" class="button" v-on:click="getBalance">
-          Get Balance
+        <!-- <button v-if="balance === null" class="button" v-on:click="getBalance">
+          Get ETH Balance
         </button>
         <div v-else>
           <hr />
@@ -16,6 +16,27 @@
           <hr />
           <h2>Balance: {{ balance }} ETH</h2>
           <hr />
+        </div> -->
+
+        <div>
+          <h3>Get TOKEN Balance</h3>
+          <br />
+          <select v-model="selectedToken">
+            <option disabled value="TOKEN">Please Select</option>
+            <option>ETH</option>
+            <option>HEX</option>
+            <option>SHIB</option>
+          </select>
+
+          <br />
+          <br />
+          <button v-on:click="getTokenBalance" class="button">
+            Get Balance
+          </button>
+
+          <br />
+          <br />
+          <p>Balance of {{ selectedToken }} is {{ tokenBalance }}</p>
         </div>
       </div>
     </div>
@@ -33,6 +54,8 @@ export default {
         : false,
       balance: null,
       chainId: null,
+      selectedToken: null,
+      tokenBalance: null,
     };
   },
   methods: {
@@ -57,12 +80,48 @@ export default {
       }
     },
     async getBalance() {
-      let web3 = new Web3(window.ethereum);
-      let accounts = await web3.eth.getAccounts();
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
       let chainId = await web3.eth.getChainId();
       let balance = await web3.eth.getBalance(accounts[0]);
       this.chainId = chainId;
       this.balance = await web3.utils.fromWei(balance, "ether"); //from Wei to ETH
+    },
+    async getTokenBalance() {
+      const tokenAddress = {
+        ETH: "0x2Cdaa8a351DFc17657C69cd79024a0d2ad504d39",
+        HEX: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+        SHIB: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+      };
+      const tokenABI = [
+        {
+          constant: true,
+          inputs: [
+            {
+              name: "_owner",
+              type: "address",
+            },
+          ],
+          name: "balanceOf",
+          outputs: [
+            {
+              name: "balance",
+              type: "uint256",
+            },
+          ],
+          payable: false,
+          type: "function",
+        },
+      ];
+
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.getAccounts();
+      let tokenInst = new web3.eth.Contract(
+        tokenABI,
+        tokenAddress[this.selectedToken]
+      );
+      this.tokenBalance = await tokenInst.methods.balanceOf(accounts[0]).call();
     },
   },
 };
